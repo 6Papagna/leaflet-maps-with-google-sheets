e9c7386d143de903c51112d91cd5bd4010d00f63
6Papagna <69843159+6Papagna@users.noreply.github.com>
2020-11-02 09:52:46 +0100

    Add files via upload

---
+++ porti.js
@@ -0,0 +1,3 @@
+//var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Wqf7Giy77W5bhoCnxp9_eGyAaFwS_RBAS18r8YoQVvM/edit#gid=0';
+
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1VagJHqEHp-9cIcNllxy2P_73voe8VhdRUrthd2NX6sk/edit#gid=0';
